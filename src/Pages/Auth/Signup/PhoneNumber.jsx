e7902a09149c343dd6c92937d6cfc4f254b5295d
Donald2023-source <donalddyusuf@gmail.com>
2025-01-25 15:29:06 -0800

    Sign up Phone Number page

--- src/Pages/Auth/Signup/PhoneNumber.jsx
+++ src/Pages/Auth/Signup/PhoneNumber.jsx
@@ -8,32 +8,34 @@ import { Link } from 'react-router-dom'
 
 const PhoneNumber = () => {
   return (
-    <div className='w-full h-screen flex items-center gap-3'>
-        <div className='w-1/2 h-full border' >
+    <div className='w-full h-screen flex items-center relative gap-3'>
+        <div className='lg:w-[60%] w-full h-full border' >
             <img className='h-full object-cover w-full' src={img1} alt='sign up img'/>
         </div>
 
-        <div className='w-1/2'>
-            <div>
-                <img className='h-10' src={logo} alt='logo'/>
-                <h2 className='font-semibold text-2xl'>Hello! Welcome</h2>
-                <p>First Add your Phone Number</p>   
+        <div className='lg:w-[40%] md:w-[70%] w-fit lg:static left-0 right-0 absolute h-full flex flex-col items-center justify-center px-4 top-0 z-50 mx-auto'>
+            <div className='lg:w-[80%] shadow-lg justify-center bg-white lg:mx-auto px-4 rounded-lg py-8 flex flex-col gap-12 items-center'>
+                <div className='flex flex-col gap-2'>
+                  <img className='h-10' src={logo} alt='logo'/>
+                  <h2 className='font-semibold text-2xl'>Hello! Welcome</h2>
+                  <p>First Add your Phone Number</p>   
+                </div>
 
-                <div>
+                <div className='flex flex-col gap-4'>
                    <form action="">
-                      <fieldset className='flex flex-col'>
-                        <label htmlFor="">Mobile Number</label>
-                        <span>
-                          <BiPhone />
-                          <input className='border py-2 px-3' type="text" placeholder='Enter your mobile number' />
+                      <fieldset className='flex flex-col rounded-lg relative border p-2 '>
+                        <label className='text-sm absolute -top-2 left-5 bg-white px-3' htmlFor="">Mobile Number</label>
+                        <span className='flex items-center'>
+                          <BiPhone className='text-gray-400' />
+                          <input className='outline-none py-2 px-3' type="text" placeholder='Enter your mobile number' />
                         </span>
                       </fieldset>
                    </form>
 
-                   <p>By continuing, you confirm ownership of this phone number and agree to receive automated texts to verify it. Standard message and data rates may apply.</p>
+                   <p className='text-sm text-gray-400'>By continuing, you confirm ownership of this phone number and agree to receive automated texts to verify it. Standard message and data rates may apply.</p>
 
-                   <button>Continue</button>
-                   <p>Already Have an Account? <Link>Login</Link></p>
+                   <button className='p-3 border w-[80%] bg-mainColor font-semibold flex items-center justify-center mx-auto'>Continue</button>
+                   <p className='text-center'>Already Have an Account? <Link className='text-myGreen font-semibold'>Login</Link></p>
                 </div>
             </div>
         </div>
