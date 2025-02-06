b0ac3355945a3b2240e091022d8903e3d21ba523
Donald2023-source <donalddyusuf@gmail.com>
2025-02-06 12:28:50 -0800

    Agent Login

--- src/Pages/Auth/Signup/Agentlogin.jsx
+++ src/Pages/Auth/Signup/Agentlogin.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import img1 from '../../../assets/image 4.png'
 import logo from '../../../assets/logo.jpg'
+import { BiPhone } from 'react-icons/bi'
 const Agentlogin = () => {
   return (
     <div className='w-full'>
@@ -8,7 +9,7 @@ const Agentlogin = () => {
         <img className='w-full object-cover' src={img1} alt='Bg img' />
       </div>
 
-        <div className='lg:w-[60%] md:w-[70%] w-[88%] lg:absolute left-0 right-0 absolute h-full flex flex-col items-center justify-center px-1 top-0 z-50 mx-auto'>
+        <div className='lg:w-[40%] md:w-[70%] w-[88%] lg:absolute left-0 right-0 absolute h-full flex flex-col items-center justify-center px-1 top-0 z-50 mx-auto'>
             <div className='lg:w-[80%] w-full shadow-lg justify-center bg-white lg:mx-auto px-4 rounded-lg py-5 md:py-8 flex flex-col gap-8 items-center'>
                 <div className='flex flex-col gap-2'>
                     <img className='h-10' src={logo} alt='logo'/>
@@ -17,11 +18,29 @@ const Agentlogin = () => {
                 </div>
 
                 <div className='w-full flex flex-col items-center'>
-                    <form className='w-[50%]' action="">
-                        <fieldset className='border p-2 rounded-lg'>
-                            <legend className='mx-3 px-1'>Agent Id</legend>
-                            <input className='h-full flex items-center justify-center w-full outline-none' type="text" />
+                    <form className='w-[90%] flex flex-col gap-5' action="">
+                        <fieldset className='flex flex-col rounded-lg relative border px-2 pb-2'>
+                            <legend className='px-4 text-gray-500 text-sm'>Full Name</legend>
+                                <span className='flex items-center'>    
+                                    <BiPhone className='text-gray-400' />
+                                    <input className='outline-none  px-3' type="text" placeholder='Enter your full name' />
+                                </span>
                         </fieldset>
+
+                        <fieldset className='flex flex-col rounded-lg relative border px-2 pb-2'>
+                            <legend className='px-4 text-gray-500 text-sm'>Full Name</legend>
+                                <span className='flex items-center'>    
+                                    <BiPhone className='text-gray-400' />
+                                    <input className='outline-none  px-3' type="text" placeholder='Enter your full name' />
+                                </span>
+                        </fieldset>
+
+                        <div>
+                            <fieldset>
+                                <input type="checkbox" />
+                                <p>Remeber Me</p>
+                            </fieldset>
+                        </div>
                     </form>
                 </div>
             </div>
