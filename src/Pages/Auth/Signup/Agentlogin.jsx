1ceb6cd0dfe1d770b93a81630d30ee50c11295f3
Donald2023-source <donalddyusuf@gmail.com>
2025-02-06 12:38:33 -0800

    agent login page

--- src/Pages/Auth/Signup/Agentlogin.jsx
+++ src/Pages/Auth/Signup/Agentlogin.jsx
@@ -4,44 +4,50 @@ import logo from '../../../assets/logo.jpg'
 import { BiPhone } from 'react-icons/bi'
 const Agentlogin = () => {
   return (
-    <div className='w-full'>
-      <div className='w-full relative'>
-        <img className='w-full object-cover' src={img1} alt='Bg img' />
+    <div className='w-full overflow-hidden h-full'>
+      <div className='w-full relative h-full'>
+        <img className='w-full object-cover h-full' src={img1} alt='Bg img' />
       </div>
 
-        <div className='lg:w-[40%] md:w-[70%] w-[88%] lg:absolute left-0 right-0 absolute h-full flex flex-col items-center justify-center px-1 top-0 z-50 mx-auto'>
-            <div className='lg:w-[80%] w-full shadow-lg justify-center bg-white lg:mx-auto px-4 rounded-lg py-5 md:py-8 flex flex-col gap-8 items-center'>
+        <div className='lg:w-[45%] md:w-[70%] w-[88%] lg:absolute left-0 right-0 absolute h-full flex flex-col items-center justify-center px-1 top-0 z-50 mx-auto'>
+            <div className='lg:w-[80%] w-full shadow-lg justify-center bg-white lg:mx-auto px-4 rounded-lg py-5 md:py-8 flex flex-col gap-10 items-center'>
                 <div className='flex flex-col gap-2'>
                     <img className='h-10' src={logo} alt='logo'/>
-                    <h2 className='font-semibold text-2xl'>Set Up Your Profile</h2>
-                    <p>The Info needs to be accurate</p>   
+                    <h2 className='font-semibold text-2xl'>Agent Login</h2>
+
                 </div>
 
                 <div className='w-full flex flex-col items-center'>
-                    <form className='w-[90%] flex flex-col gap-5' action="">
+                    <form className='w-[90%] flex flex-col gap-6' action="">
                         <fieldset className='flex flex-col rounded-lg relative border px-2 pb-2'>
-                            <legend className='px-4 text-gray-500 text-sm'>Full Name</legend>
+                            <legend className='px-4 text-gray-500 text-sm'>Agent ID</legend>
                                 <span className='flex items-center'>    
                                     <BiPhone className='text-gray-400' />
-                                    <input className='outline-none  px-3' type="text" placeholder='Enter your full name' />
+                                    <input className='outline-none  px-3' type="text" placeholder='Enter your Agent ID' />
                                 </span>
                         </fieldset>
 
                         <fieldset className='flex flex-col rounded-lg relative border px-2 pb-2'>
-                            <legend className='px-4 text-gray-500 text-sm'>Full Name</legend>
+                            <legend className='px-4 text-gray-500 text-sm'>Password</legend>
                                 <span className='flex items-center'>    
                                     <BiPhone className='text-gray-400' />
-                                    <input className='outline-none  px-3' type="text" placeholder='Enter your full name' />
+                                    <input className='outline-none  px-3' type="text" placeholder='Enter your password' />
                                 </span>
                         </fieldset>
 
-                        <div>
-                            <fieldset>
+                        <div className='flex items-center justify-between'>
+                            <fieldset className='flex gap-2'>
                                 <input type="checkbox" />
                                 <p>Remeber Me</p>
                             </fieldset>
+
+                            <p className='text-myGreen cursor-pointer py-4'>Forgot ID or Password</p>
+                            
                         </div>
                     </form>
+
+                    <button className='p-3 border w-[80%] bg-mainColor font-semibold flex items-center justify-center mx-auto'>Continue</button>
+
                 </div>
             </div>
         </div> 
