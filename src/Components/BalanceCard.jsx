30e42a562c0aad5aab78c567374557a3a5edfc8a
Donald2023-source <donalddyusuf@gmail.com>
2025-02-11 17:23:23 -0800

    Balance card

--- src/Components/BalanceCard.jsx
+++ src/Components/BalanceCard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import { FaEye } from 'react-icons/fa'
+import Naira from ''
 const Cards = () => {
   return (
     <div>
-      
+      <div>
+        <h2>Current Balance</h2>
+        <FaEye />
+      </div>
+
+    <div>
+      <img src={Naira} alt="" />
+    </div>
+
     </div>
   )
 }
