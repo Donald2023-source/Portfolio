953e6645a516896eaddcdba5030f145374d39755
Donald2023-source <donalddyusuf@gmail.com>
2025-02-17 16:44:33 -0800

    Balance Card

--- src/Pages/UserDashboard/Pages/Dashboard.jsx
+++ src/Pages/UserDashboard/Pages/Dashboard.jsx
@@ -6,8 +6,10 @@ const Dashboard = () => {
         <div className="py-2 w-full bg-silver">
             <TopBar name={'Sonia'} />
 
-            <div>
-                <span className="lg:block hidden md:block"><BalanceCard /></span>
+            <div className="flex w-full">
+                <div className="w-full flex flex-col gap-2 py-3">
+                    <span className="lg:block hidden md:block"><BalanceCard /></span>
+                </div>
             </div>
         </div>
     )
