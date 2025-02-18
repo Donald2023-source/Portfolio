52492ae29a3bf321c07239c866a29259727459c2
Donald2023-source <donalddyusuf@gmail.com>
2025-02-17 16:56:24 -0800

    Profile Bar

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Profile = () => {
+    const date = new Date()
+    console.log(date)
+
+  return (
+    <div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Profile
--- src/Pages/UserDashboard/Pages/Dashboard.jsx
+++ src/Pages/UserDashboard/Pages/Dashboard.jsx
@@ -1,9 +1,11 @@
 import BalanceCard from "../../../Components/BalanceCard"
+import Profile from "../../../Components/Profile"
 import TopBar from "../../../Components/TopBar"
 
 const Dashboard = () => {
     return (
         <div className="py-2 w-full bg-silver">
+            <Profile />
             <TopBar name={'Sonia'} />
 
             <div className="flex w-full">
