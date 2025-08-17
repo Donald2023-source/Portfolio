a9d752898195281535f7e8cf7abc6fa6ebc8a3fb
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-17 14:22:08 -0700

    changes

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -3,12 +3,16 @@ import img from "../../public/Appolo.png";
 import { FaEye } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const ProjectCard = ({name, href, description, image}) => {
+const ProjectCard = ({ name, href, description, image }) => {
   return (
     <div className="w-72 relative w-[20rem] ">
       <div className="relative rounded-2xl overflow-hidden">
-        <img className="h-64 w-full object-cover rounded-2xl" src={image} alt="" />
-
+        <img
+          className="h-64 w-full object-cover rounded-2xl"
+          src={image}
+          alt=""
+        />
+        <div className="absolute inset-0 rounded-2xl  bg-black/30 h-full w-full"></div>
         <Link
           to={href}
           className="absolute bg-black hover:cursor-pointer opacity-0 hover:opacity-65 transition-all w-full text-2xl top-0 h-full items-center flex justify-center left-0 right-0 mx-auto"
@@ -18,9 +22,7 @@ const ProjectCard = ({name, href, description, image}) => {
       </div>
       <div>
         <h1 className="text-xl font-semibold py-2">{name}</h1>
-        <p className="text-sm leading-7">
-          {description}
-        </p>
+        <p className="text-sm leading-7 text-textGray">{description}</p>
       </div>
 
       <div></div>
