da9929c1cc8588ba24e99701c57be37886af4f78
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-18 05:13:02 -0700

    changes

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,36 @@
+const ContactForm =() => {
+  const [state, handleSubmit] = useForm("xpwleobb");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
+} 
+export default ContactForm;
