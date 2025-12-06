bcae487a6772f589ea01d25e8e00beb00eb8cb84
Donald2023-source <donalddyusuf@gmail.com>
2025-12-06 13:42:54 +0100

    style: alignments

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -26,7 +26,7 @@ const Hero = () => {
       <a
         href="/Donald-Resume.pdf"
         download="Donald_Yusuf.pdf"
-        className="px-1 inline-block border border-gray-100/40 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
+        className="px-1 inline-block border text-center border-gray-100/40 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
       >
         <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
           Download CV
